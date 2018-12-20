54417642ffe9d1910b19521faf9b70b094cd2b52
Abdulla Mian <miana@claycorp.com>
2018-12-20 11:33:27 -0600

    tests for 1 -20

--- FizzBuzzKata/FizzBuzzLibrary/FizzBuzzer.cs
+++ FizzBuzzKata/FizzBuzzLibrary/FizzBuzzer.cs
@@ -6,17 +6,19 @@ namespace FizzBuzzLibrary
     {
         public static string GetValue(int input)
         {
+            if (input % 5 == 0 && input % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
             if (input % 3 == 0)
             {
                 return "Fizz";
             }
 
-            if (input == 5)
+            if (input % 5 == 0)
             {
                 return "Buzz";
             }
-
-
             else
             {
                 return input.ToString();
--- FizzBuzzKata/FizzBuzzTestLibrary/FizzBuzzerTests.cs
+++ FizzBuzzKata/FizzBuzzTestLibrary/FizzBuzzerTests.cs
@@ -10,6 +10,12 @@ namespace FizzBuzzTestLibrary
         [TestCase(1)]
         [TestCase(2)]
         [TestCase(4)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(11)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(19)]
         public void Buzzer_WhenDefault_ReturnsInput(int input)
         {
             string output = FizzBuzzer.GetValue(input);
@@ -18,18 +24,29 @@ namespace FizzBuzzTestLibrary
 
         [TestCase(3)]
         [TestCase(6)]
-        public void Buzzer_When3_ReturnsFizz(int input)
+        [TestCase(9)]
+        [TestCase(12)]
+        [TestCase(18)]
+        public void Buzzer_WhenDiv3_ReturnsFizz(int input)
         {
             string output = FizzBuzzer.GetValue(input);
             Assert.AreEqual("Fizz", output);
         }
 
-        [TestCase]
-        public void Buzzer_When5_ReturnsBuzz()
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void Buzzer_WhenDiv5_ReturnsBuzz(int input)
         {
-            int input = 5;
             string output = FizzBuzzer.GetValue(input);
             Assert.AreEqual("Buzz", output);
+        }
+
+        [TestCase(15)]
+        public void Buzzer_WhenDiv3AndDiv5_ReturnsFizzBuzz(int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
         }
        
     }
