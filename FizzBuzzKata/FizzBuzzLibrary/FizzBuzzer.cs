b8f97c03804150d8cb6a0a7fce1c14e8130aa8de
Abdulla Mian <miana@claycorp.com>
2018-12-20 11:00:23 -0600

    created tests

---
+++ FizzBuzzKata/FizzBuzzLibrary/FizzBuzzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzzLibrary
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            if (input == 3)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return input.ToString();
+            }
+            
+        }
+    }
+}
---
+++ FizzBuzzKata/FizzBuzzTestLibrary/FizzBuzzerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FizzBuzzLibrary;
+using NUnit.Framework;
+
+namespace FizzBuzzTestLibrary
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [TestCase]
+       public void Buzzer_When1_Returns1()
+        {
+            int input = 1;
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("1", output);
+        }
+
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        [TestCase(6)]
+        public void Buzzer_When2_Returns2(int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual(input.ToString(), output);
+        }
+
+        [TestCase]
+        public void Buzzer_When3_ReturnsFizz()
+        {
+            int input = 3;
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+       
+    }
+}
