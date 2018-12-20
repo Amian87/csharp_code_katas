13cae3e816ffe3cbb6c3a505ce6750a7fc9ade81
Abdulla Mian <miana@claycorp.com>
2018-12-20 11:17:54 -0600

    refactored tests

--- FizzBuzzKata/FizzBuzzLibrary/FizzBuzzer.cs
+++ FizzBuzzKata/FizzBuzzLibrary/FizzBuzzer.cs
@@ -6,10 +6,17 @@ namespace FizzBuzzLibrary
     {
         public static string GetValue(int input)
         {
-            if (input == 3)
+            if (input % 3 == 0)
             {
                 return "Fizz";
             }
+
+            if (input == 5)
+            {
+                return "Buzz";
+            }
+
+
             else
             {
                 return input.ToString();
