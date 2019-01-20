d389202f5bdf5a0aedf663ba87799d2f0de44fcc
Abdulla Mian <miana@claycorp.com>
2019-01-20 12:19:54 -0600

    practice

--- FizzBuzzKata/FizzBuzz/Program.cs
+++ FizzBuzzKata/FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzBuzzerLibrary;
+using System;
 
 namespace FizzBuzz
 {
@@ -6,7 +7,11 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            for (var i = 1; i < 100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ FizzBuzzKata/FizzBuzzTestLibrary/FizzBuzzTest.cs
@@ -0,0 +1,56 @@
+﻿using FizzBuzzerLibrary;
+using NUnit.Framework;
+using System;
+
+namespace FizzBuzzTestLibrary
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(11)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(16)]
+        [TestCase(17)]
+        [TestCase(19)]
+        public void When_NOT_Divisible_By_3_And_5_Return_Input(int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual(input.ToString(), output);
+        }
+        
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(12)]
+        [TestCase(18)]
+        public void When_Divisble_By_3_Return_Fizz(int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void When_Divisible_By_5_Return_Buzz(int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [TestCase(15)]
+        public void When_Divisbile_By_5_And_3_Return_FizzBuzz(int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
+        }
+
+    }
+}
+            
